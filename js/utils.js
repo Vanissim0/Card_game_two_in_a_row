6cffa52fd51791a5c8854313528a4f6d21c1f3f3
IVaN Ivanovi4 <eng.vano@yandex.ru>
2022-12-18 00:15:31 +0300

    Initial commit

---
+++ js/utils.js
@@ -0,0 +1,47 @@
+export const shuffle = (array) => {
+    let currentIndex = array.length, randomIndex;
+
+    while (currentIndex != 0) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
+  // Used like so
+  var arr = [2, 11, 37, 42];
+  shuffle(arr);
+  console.log(arr);
+
+export const duplicateArray = (array) => array.reduce((res, current) => res.concat([current, current]), []);
+
+export const createIconsArray = (initialCount) => {
+    const cardsIcons = [
+        'compass',
+        'play',
+        'cloud',
+        'bolt',
+        'stop',
+        'cogs',
+        'atom',
+        'basketball-ball'
+    ];
+
+    switch (initialCount) {
+        case 10:
+            return cardsIcons.slice(0, 5);
+        case 12:
+            return cardsIcons.slice(0, 6);
+        case 14:
+            return cardsIcons.slice(0, 7);
+        case 16:
+            return cardsIcons;    
+        default:
+            break;
+    }
+}
